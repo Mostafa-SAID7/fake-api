4d83a1f9a28d98304ac2d71703e67ddcc2f5c742
M.sSaid said <m.ssaid356@gmail.com>
2025-01-14 19:26:07 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+fetch("https://dummyjson.com/products")
+  .then((res) => res.json())
+  .then((data) => {
+    if (!Array.isArray(data)) {
+      if (data && Array.isArray(data.products)) {
+        data = data.products;
+      } else {
+        console.log(data);
+      }
+    }
+    const productContainer = document.getElementById("product-container");
+    data.forEach((product) => {
+      const cardDiv = document.createElement("div");
+      cardDiv.classList.add("col-md-4", "mb-3");
+      cardDiv.innerHTML = `
+        <div class="card" style="box-shadow:10px 10px 15px gray">
+          <img src="${product.images[0]}" class="card-img-top" width="300px" height="150px" alt="${product.title}">
+  <div class="card-body">
+    <h5 class="card-title">${product.title}</h5>
+    <p class="card-text">${product.description}</p>
+    <a href="#" class="btn btn-primary">Add To Card</a>
+  </div>
+        </div>
+        `;
+      productContainer.appendChild(cardDiv);
+    });
+  });
